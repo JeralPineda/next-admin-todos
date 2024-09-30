dbe09d5a63d8b19b85e89e9ef26e0558e17c9ee0
jcpmbhc <jpineda@banhcafe.hn>
2024-09-30 12:53:15 -0600

    fix: agregando paginacion simple

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,28 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany();
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take")) ?? "10";
+  const skip = Number(searchParams.get("skip")) ?? "0";
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "Take tiene que ser un numeró" },
+      { status: 400 },
+    );
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: "Skip tiene que ser un numeró" },
+      { status: 400 },
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take, // +take para convertir a numero
+    skip,
+  });
 
   return NextResponse.json(todos);
 }
