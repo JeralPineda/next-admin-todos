24cb2c66c81c3f20e66a864aeb946da2dd2fc0bd
Jeral Pineda <jeral.cris@gmail.com>
2024-10-19 10:46:51 -0600

    fix: agregando configuracion de clases no computadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,16 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
+  // Nombres de clases computadas
+  safelist: [
+    {
+      pattern: /grid-cols-./,
+    },
+  ],
 };
