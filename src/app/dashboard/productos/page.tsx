b6406c25ad0cc157519afe9a0b4d737eeaaa595c
Jeral Pineda <jeral.cris@gmail.com>
2024-10-19 13:07:42 -0600

    fix: mostrar el listado de productos

--- src/app/dashboard/productos/page.tsx
+++ src/app/dashboard/productos/page.tsx
@@ -1,9 +1,12 @@
 import { ProductCard } from "@/productos/components/product-card";
+import { products } from "@/productos/data/products";
 
 export default function page() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-      <ProductCard />
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
     </div>
   );
 }
