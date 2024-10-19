8fb3cc2bae9f55ccea13b39d4c45fc0a20dff942
Jeral Pineda <jeral.cris@gmail.com>
2024-10-19 11:15:15 -0600

    fix: server side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/components/tab-bar";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies Page",
@@ -6,11 +7,17 @@ export const metadata = {
 };
 
 export default function page() {
+  const cookiesStore = cookies();
+  const cookieTab = cookiesStore.get("selectedTab")?.value ?? "1";
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar
+          //? También para comvertir el string a numero se puede agregar un + antes de la variable +cookieTab
+          currentTab={Number(cookieTab)}
+        />
       </div>
     </div>
   );
