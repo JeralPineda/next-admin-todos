0a16a5c87b6ae8245a68249b0d96cc7dbeab47ce
Jeral Pineda <jeral.cris@gmail.com>
2024-10-19 19:04:45 -0600

    fix: shopping-cart - client side

--- src/productos/components/product-card.tsx
+++ src/productos/components/product-card.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
 import { Star } from "./star";
+import { addProductToCart } from "@/shopping-cart/actions";
 
 interface ProductCardProps {
   id: string;
@@ -17,6 +20,10 @@ export const ProductCard = ({
   rating,
   image,
 }: ProductCardProps) => {
+  const onAddToCart = () => {
+    addProductToCart(id);
+  };
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 border-gray-100">
       {/* Product Image */}
@@ -56,7 +63,10 @@ export const ProductCard = ({
           <span className="text-3xl font-bold text-white">${price}</span>
 
           <div className="flex">
-            <button className="text-white mr-2 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+            <button
+              onClick={onAddToCart}
+              className="text-white mr-2 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+            >
               <IoAddCircleOutline size={25} />
             </button>
             <button className="text-white  hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
---
+++ src/shopping-cart/actions/index.ts
@@ -0,0 +1,29 @@
+// "use client";
+
+import { getCookie, hasCookie, setCookie } from "cookies-next";
+
+/** 
+ "uui-123-1": 4,
+ "uui-123-2": 5,
+ * */
+
+export const getCookieCart = (): { [id: string]: number } => {
+  if (hasCookie("cart")) {
+    const cookieCart = JSON.parse((getCookie("cart") as string) ?? "{}");
+    return cookieCart;
+  }
+
+  return {};
+};
+
+export const addProductToCart = (id: string) => {
+  let cookieCart = getCookieCart();
+
+  if (cookieCart[id]) {
+    cookieCart[id] += 1;
+  } else {
+    cookieCart[id] = 1;
+  }
+
+  setCookie("cart", JSON.stringify(cookieCart));
+};
