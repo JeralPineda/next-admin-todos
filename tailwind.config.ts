acbc4b1a5543ca8fb4d920ebd96c3a22a41a9ab3
Jeral Pineda <jeral.cris@gmail.com>
2024-10-27 11:18:18 -0600

    fix: add directory in tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/todos/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/productos/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
