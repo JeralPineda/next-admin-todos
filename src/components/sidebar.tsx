d62a64d962e6c0ab52a00dd5191a5490bfddc4c1
Jeral Pineda <jeral.cris@gmail.com>
2024-10-28 20:52:08 -0600

    fix: mostrar informacion del usuario

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,6 +9,8 @@ import {
   IoBasketOutline,
 } from "react-icons/io5";
 import { SidebarItem, SidebarItemProps } from "./sidebar-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItems: SidebarItemProps[] = [
   {
@@ -38,7 +40,14 @@ const menuItems: SidebarItemProps[] = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  const avatarUrl =
+    session?.user?.image ??
+    "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp";
+  const userNAme = session?.user?.name ?? "No Name";
+  //TODO: const userRole = session?.user?.role ?? "No Role";
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -58,14 +67,14 @@ export const Sidebar = () => {
 
         <div className="mt-8 text-center">
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={avatarUrl}
             width={150}
             height={150}
-            alt=""
+            alt={userNAme}
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {userNAme}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
