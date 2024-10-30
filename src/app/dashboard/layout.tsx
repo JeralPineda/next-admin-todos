2b30d49183edc0e0e8c442d2206271533379ef09
Jeral Pineda <jeral.cris@gmail.com>
2024-10-29 22:25:11 -0600

    fix: signOut y signIn

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "@/components/sidebar";
+import { Sidebar } from "@/components/sidebar/sidebar";
 import { TopMenu } from "@/components/top-menu";
 
 export default function DashboardLayout({
