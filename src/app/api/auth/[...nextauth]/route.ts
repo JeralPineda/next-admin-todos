46c98334c8819a415d1acc198510341c8ae91ef9
Jeral Pineda <jeral.cris@gmail.com>
2024-10-30 22:17:30 -0600

    fix: CredentialsProvider para ingresar con email y password

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,10 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import prisma from "@/lib/prisma";
+import { signInEmailPassword } from "@/auth/actions/auth-actions";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -20,7 +22,35 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
-    // ...add more providers here
+
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: {
+          label: "Correo",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+        password: {
+          label: "Contraseña",
+          type: "password",
+          placeholder: "******",
+        },
+      },
+      async authorize(credentials, req) {
+        const user = await signInEmailPassword(
+          credentials!.email,
+          credentials!.password,
+        );
+
+        if (user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
   ],
 
   session: {
