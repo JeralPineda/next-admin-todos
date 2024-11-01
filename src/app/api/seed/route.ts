6b8536a463248ec4cf3aad3dc09bf897b9cf6e26
Jeral Pineda <jeral.cris@gmail.com>
2024-10-31 22:10:07 -0600

    fix: relaciones y semilla de base de la datos

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,18 +1,37 @@
-import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
   await prisma.todo.deleteMany(); // delete from * todo
+  await prisma.user.deleteMany(); // delete from * user
 
-  await prisma.todo.createMany({
-    data: [
-      { description: "Piedra del alma", complete: true },
-      { description: "Piedra del poder" },
-      { description: "Piedra del tiempo" },
-      { description: "Piedra del espacio" },
-      { description: "Piedra de la realidad" },
-    ],
+  await prisma.user.create({
+    data: {
+      email: "test1@test.com",
+      password: bcrypt.hashSync("123456"),
+      roles: ["admin", "client", "super-user"],
+      todos: {
+        create: [
+          { description: "Piedra del alma", complete: true },
+          { description: "Piedra del poder" },
+          { description: "Piedra del tiempo" },
+          { description: "Piedra del espacio" },
+          { description: "Piedra de la realidad" },
+        ],
+      },
+    },
   });
+
+  // await prisma.todo.createMany({
+  //   data: [
+  //     { description: "Piedra del alma", complete: true },
+  //     { description: "Piedra del poder" },
+  //     { description: "Piedra del tiempo" },
+  //     { description: "Piedra del espacio" },
+  //     { description: "Piedra de la realidad" },
+  //   ],
+  // });
 
   return NextResponse.json({
     message: "Seed Executed",
